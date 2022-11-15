9713e61a07af8f796854b34c678ed2caadf12e00
cristian <cristiangalvezcaravaca@gmail.com>
2022-11-15 12:11:43 +0100

    Creamos el constructor copia

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,29 +1,42 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-private int fila;
-private char Columna;
+	private int fila;
+	private char columna;
 
-public int getFila() {
-	return fila;
-}
-private void setFila(int fila) {
-	if (fila >= 1 && fila <= 8) {
-		this.fila = fila;
-	}else {
-		throw new IllegalArgumentException("La fila se sale de los valores permitidos");
+	public int getFila() {
+		return fila;
 	}
-}
-public char getColumna() {
-	return Columna;
-}
-private void setColumna(char columna) {
-	if (columna >= 'a' && columna <= 'h') {
-	this.Columna = columna;
-}else {
-	throw new IllegalArgumentException("La columna se sale de los valores permitidos");
-	}
-}
 
+	private void setFila(int fila) {
+		if (fila >= 1 && fila <= 8) {
+			this.fila = fila;
+		} else {
+			throw new IllegalArgumentException("La fila se sale de los valores permitidos");
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("La columna se sale de los valores permitidos");
+		}
+
+	}
+
+	public Posicion(int fila, char columna) {
+		setColumna(columna);
+		setFila(fila);
+	}
+	
+	public Posicion(Posicion posicion) {
+	fila = posicion.getFila();
+	columna = posicion.getColumna();
+	}
 
 }

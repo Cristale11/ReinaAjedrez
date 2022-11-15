4ede86b791adf658eac1a488fd70bc37d9d318b0
cristian <cristiangalvezcaravaca@gmail.com>
2022-11-15 11:09:52 +0100

    He agregado los get y set de columna y fila.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,6 +1,29 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-int fila;
-char Columna;
+private int fila;
+private char Columna;
+
+public int getFila() {
+	return fila;
+}
+private void setFila(int fila) {
+	if (fila >= 1 && fila <= 8) {
+		this.fila = fila;
+	}else {
+		throw new IllegalArgumentException("La fila se sale de los valores permitidos");
+	}
+}
+public char getColumna() {
+	return Columna;
+}
+private void setColumna(char columna) {
+	if (columna >= 'a' && columna <= 'h') {
+	this.Columna = columna;
+}else {
+	throw new IllegalArgumentException("La columna se sale de los valores permitidos");
+	}
+}
+
+
 }

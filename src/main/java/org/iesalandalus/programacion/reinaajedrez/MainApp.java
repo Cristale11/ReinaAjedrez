521600f3bc19ffa220e360779d46b83298222098
cristian <cristiangalvezcaravaca@gmail.com>
2022-11-17 21:49:16 +0100

    Agregamos el metodo crearReinaColor

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,14 +1,19 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
 	private Reina reina;
+
 	private static void crearReinaDefecto() {
 		Reina reinadefecto = new Reina();
-		
+
+	}
+	private static void crearReinaColor() {
+		Reina reinanegra = new Reina();
 	}
 	public static void main(String[] args) {
-		
+
 	}
 }

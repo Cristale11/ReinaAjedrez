d1327a3ffff224b3a0469fc1e204a01f49db4061
cristian <cristiangalvezcaravaca@gmail.com>
2022-11-17 21:40:34 +0100

    Creamos el metodo crearReinaDefecto

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,9 +1,14 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
 	private Reina reina;
-
+	private static void crearReinaDefecto() {
+		Reina reinadefecto = new Reina();
+		
+	}
 	public static void main(String[] args) {
-
+		
 	}
 }

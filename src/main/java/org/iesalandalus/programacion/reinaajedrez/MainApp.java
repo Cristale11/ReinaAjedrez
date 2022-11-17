1feed25aac8b721e0264d73c699b22f9f3f259dd
cristian <cristiangalvezcaravaca@gmail.com>
2022-11-17 22:45:36 +0100

    Agregamos el metodo mostrarReina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -9,40 +9,6 @@ public class MainApp {
 	private Reina reina;
 	private Consola consola;
 
-	private static void ejecutarOpcion() {
-		Scanner sc = new Scanner(System.in);
-		int opcion;
-		Consola.mostrarMenu();
-		Consola.elegirOpcionMenu();
-		opcion = sc.nextInt();
-		do {
-		switch (opcion) {
-		case 1:
-			crearReinaDefecto();
-			break;
-		case 2:
-			Consola.elegirOpcion();
-			opcion = sc.nextInt();
-			switch (opcion) {
-			case 1:
-				crearReinaDefecto();
-				break;
-			case 2:
-				crearReinaColor();
-				break;
-			}
-			break;
-		case 3:
-			mover();
-			break;
-		case 4:
-			Consola.despedirse();
-			break;
-		}
-		}while (opcion != 4);
-
-	}
-
 	private static void crearReinaDefecto() {
 		Reina reinadefecto = new Reina();
 
@@ -50,14 +16,55 @@ public class MainApp {
 
 	private static void crearReinaColor() {
 		Reina reinanegra = new Reina(Color.NEGRO);
+		reinanegra.getColor();
 	}
-	private static void mover() {
+
+	private static void mover() { // Preguntar duda
 		Consola.mostrarMenuDirecciones();
 		Consola.elegirDireccion();
-		
+
 	}
+
+	private static void mostrarReina() { // Preguntar duda
+
+	}
+
+	private static void ejecutarOpcion() {
+		Scanner sc = new Scanner(System.in);
+		int opcion;
+		Consola.mostrarMenu();
+		Consola.elegirOpcionMenu();
+		opcion = sc.nextInt();
+		do {
+			switch (opcion) {
+			case 1:
+				crearReinaDefecto();
+				break;
+			case 2:
+				Consola.elegirOpcion();
+				opcion = sc.nextInt();
+				switch (opcion) {
+				case 1:
+					crearReinaDefecto();
+					break;
+				case 2:
+					crearReinaColor();
+					break;
+				}
+				break;
+			case 3:
+				mover();
+				break;
+			case 4:
+				Consola.despedirse();
+				break;
+			}
+		} while (opcion != 4);
+
+	}
+
 	public static void main(String[] args) {
 		ejecutarOpcion();
-		
+
 	}
 }
